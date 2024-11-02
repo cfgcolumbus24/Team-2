114e788b943ea590f3a63c31da28862d370172ad
DEVSOG12 <oreofesolarin@gmail.com>
2024-11-02 00:01:02 +0000

    Fixed Issues with EC2

--- Backend/src/i.js
+++ Backend/src/i.js
@@ -2,17 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const csv = require('csv-parser');
 const app = express();
-const port = process.env.PORT || 3000; // Use port from env or default to 3000
-
-// Check if the app is running on an EC2 instance
-const isEC2 = process.env.PORT !== null ||  process.env.EC2_INSTANCE === 'true' || process.env.NODE_ENV === 'production';
-
-app.get('/', (req, res) => {
-
-res.json({"hi":"Hello"})
-
-});
-
+const port = 3000;
 
 app.get('/data/patient', (req, res) => {
     const results = [];
@@ -27,14 +17,6 @@ app.get('/data/patient', (req, res) => {
         });
 });
 
-// Log whether we're in EC2 or local
-if (isEC2) {
-    console.log("Running on an EC2 instance");
-} else {
-    console.log("Running locally");
-}
-
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
