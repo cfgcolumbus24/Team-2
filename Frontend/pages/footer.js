fbbcbeeac08894e65a84a06d886540fe41b2bce3
Dragontory <toryyang03@gmail.com>
2024-11-02 07:07:55 -0400

    Added links for footer

--- Frontend/pages/footer.js
+++ Frontend/pages/footer.js
@@ -16,25 +16,25 @@ export default function Footer() {
       </div>
 
       <div className={styles.mainLinks}>
-        <a href="#services" className={styles.mainLink}>Services</a>
-        <a href="#careers" className={styles.mainLink}>Careers</a>
-        <a href="#about" className={styles.mainLink}>About Us</a>
+        <a href="https://www.netcareaccess.org/services/" target="_blank" className={styles.mainLink}>Services</a>
+        <a href="https://www.netcareaccess.org/careers/" target="_blank" className={styles.mainLink}>Careers</a>
+        <a href="https://www.netcareaccess.org/about/" target="_blank" className={styles.mainLink}>About Us</a>
       </div>
 
       <div className={styles.footerColumns}>
         <div className={styles.footerColumn}>
           <h3 className={styles.columnTitle}>Crisis Help</h3>
-          <a href="#referral" className={styles.footerLink}>How to Make a Referral</a>
-          <a href="#crisis-services" className={styles.footerLink}>Crisis Services</a>
+          <a href="https://www.netcareaccess.org/?page_id=971" target="_blank" className={styles.footerLink}>How to Make a Referral</a>
+          <a href="https://www.netcareaccess.org/services/crisis-services/" target="_blank" className={styles.footerLink}>Crisis Services</a>
         </div>
 
         <div className={styles.footerColumn}>
           <h3 className={styles.columnTitle}>Resources</h3>
-          <a href="#privacy-statement" className={styles.footerLink}>Netcare Privacy Statement</a>
-          <a href="#privacy-practices" className={styles.footerLink}>Notice of Privacy Practices</a>
-          <a href="#shelter-info" className={styles.footerLink}>Community Shelter Board Information</a>
-          <a href="#formas-espanolas" className={styles.footerLink}>Formas Españolas</a>
-          <a href="#faq" className={styles.footerLink}>FAQ</a>
+          <a href="https://www.netcareaccess.org/footer/resources/netcare-privacy-statement/" target="_blank" className={styles.footerLink}>Netcare Privacy Statement</a>
+          <a href="https://www.netcareaccess.org/wp-content/uploads/2020/10/NOPP.pdf" target="_blank" className={styles.footerLink}>Notice of Privacy Practices</a>
+          <a href="https://www.netcareaccess.org/footer/resources/community-shelter-board-information/" target="_blank" className={styles.footerLink}>Community Shelter Board Information</a>
+          <a href="https://www.netcareaccess.org/footer/resources/formasespanolas/" target="_blank" className={styles.footerLink}>Formas Españolas</a>
+          <a href="https://www.netcareaccess.org/footer/resources/faq/" target="_blank" className={styles.footerLink}>FAQ</a>
         </div>
 
         <div className={styles.footerColumn}>
@@ -48,19 +48,19 @@ export default function Footer() {
       <div className={styles.footerBottom}>
         <p className={styles.copyright}>© 2024 Netcare Access</p>
         <div className={styles.footerImages}>
-          <a href="#link1" className={styles.footerImageLink}>
+          <a href="https://adamhfranklin.org/" target="_blank" className={styles.footerImageLink}>
             <img src="/image1_ADAMH.png" alt="Link 1" className={styles.footerImage} />
           </a>
-          <a href="#link2" className={styles.footerImageLink}>
+          <a href="https://www.csb.org/" target="_blank" className={styles.footerImageLink}>
             <img src="/image2_CSB.png" alt="Link 2" className={styles.footerImage} />
           </a>
-          <a href="#link3" className={styles.footerImageLink}>
+          <a href="https://humanservicechamber.org/" target="_blank" className={styles.footerImageLink}>
             <img src="/image3_HSC.png" alt="Link 3" className={styles.footerImage} />
           </a>
-          <a href="#link4" className={styles.footerImageLink}>
+          <a href="https://www.netcareaccess.org/#" target="_blank" className={styles.footerImageLink}>
             <img src="/image4_CWC.png" alt="Link 4" className={styles.footerImage} />
           </a>
-          <a href="#link5" className={styles.footerImageLink}>
+          <a href="https://www.netcareaccess.org/#" target="_blank" className={styles.footerImageLink}>
             <img src="/image5_rainbow.png" alt="Link 5" className={styles.footerImage} />
           </a>
         </div>
